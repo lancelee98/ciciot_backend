5215a0a55cd6f0e8580593430f1d25b12f30aae9
AnyISalIn <anyisalin@gmail.com>
2018-07-31 12:15:19 +0800

    update list page

--- article/views.py
+++ article/views.py
@@ -46,6 +46,22 @@ class ListView(View):
         category_name = request.GET.get('category')
         category = get_object_or_404(Category, name=category_name)
         articles = category.article_set.filter(is_public=True).all()
+
+        def get_next_categories(current_category):
+            ret = dict()
+            ret['current_category'] = current_category.name
+            category_count = Category.objects.count()
+            if current_category.id < category_count and (category_count - current_category.id) >= 2:
+                ret['next_category'] = Category.objects.get(pk=current_category.id + 1).name
+                ret['next_next_category'] = Category.objects.get(pk=current_category.id + 2).name
+            elif current_category.id < category_count and (category_count - current_category.id) == 1:
+                ret['next_category'] = Category.objects.get(pk=current_category.id + 1).name
+                ret['next_next_category'] = Category.objects.get(pk=1).name
+            else:
+                ret['next_category'] = Category.objects.get(pk=1).name
+                ret['next_next_category'] = Category.objects.get(pk=2).name
+            return ret
+
         context = {
             'articles': articles,
             'hot_articles': get_hot_articles(6),
@@ -53,4 +69,6 @@ class ListView(View):
             'tech_articles': fetch_category_articles('前沿科技', 4),
             'net_articles': fetch_category_articles('网络技术', 4),
         }
+
+        context.update(get_next_categories(category))
         return render(request, self.template_name, context)
